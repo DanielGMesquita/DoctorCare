0228ecededc594bab1b78c491319b3446336629c
Daniel <daniel_mesquita2@hotmail.com>
2022-05-08 21:03:54 -0300

    Versão Beta

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showButton()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showButton() {
+  if (scrollY > 400) {
+    returnToTop.classList.add('show')
+  } else {
+    returnToTop.classList.remove('show')
   }
 }
 
@@ -22,4 +41,27 @@ ScrollReveal({
   '#home, #home img, #home #numbers, #services, #services header, #services .card'
 )
 
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionTopReachedOrPassedTargetLine = targetLine >= sectionTop
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // Limite da seção: topo atingiu ou não o target de mudança de seção mas o fundo ainda não - indica que a seção não mudou ainda
+  const sectionBoundaries =
+    sectionTopReachedOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  // condicional
+  // acento grave serve pra colocar uma variável dentro da string
+  //${} - interpolação, dentro da chave pode colocar qualquer codigo javascript
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
 // DOM - Document Object Model - todo HTML modelado como objeto javascript
